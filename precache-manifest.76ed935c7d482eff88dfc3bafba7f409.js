50059e5dabcea9b72caa0c7a34aa1198943a3392
zionjke <vincere802@gmail.com>
2020-09-01 17:02:48 +0300

    Updates

---
+++ precache-manifest.76ed935c7d482eff88dfc3bafba7f409.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4df02c7174f029ea155f818275849641",
+    "url": "/tasks-columns/index.html"
+  },
+  {
+    "revision": "64fcc4155e0b364b3807",
+    "url": "/tasks-columns/static/css/main.6eb34e83.chunk.css"
+  },
+  {
+    "revision": "add935490e36af973e02",
+    "url": "/tasks-columns/static/js/2.f0a9a3ee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/tasks-columns/static/js/2.f0a9a3ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64fcc4155e0b364b3807",
+    "url": "/tasks-columns/static/js/main.c094be39.chunk.js"
+  },
+  {
+    "revision": "1d328883de328b70256b",
+    "url": "/tasks-columns/static/js/runtime-main.3c369faa.js"
+  },
+  {
+    "revision": "78cc821587c8145935bfc5b01738d502",
+    "url": "/tasks-columns/static/media/add.78cc8215.svg"
+  },
+  {
+    "revision": "b36a3633ba2e4be2ec54e7b20ba43f53",
+    "url": "/tasks-columns/static/media/remove.b36a3633.svg"
+  }
+]);
